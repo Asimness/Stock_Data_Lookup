66b458d886ba0edd03d002b7ac0f44805c04f6a3
Asim Anis <asimness@gmail.com>
2019-02-16 01:06:44 -0600

    Initial commit

---
+++ app/src/main/java/com/example/asim/assignment2/Assignment2ActivityOne.java
@@ -0,0 +1,132 @@
+package com.example.asim.assignment2;
+
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.util.TypedValue;
+import android.view.View;
+import android.widget.AbsListView;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import static java.net.Proxy.Type.HTTP;
+
+public class Assignment2ActivityOne extends AppCompatActivity implements StockDataDisplayFragment.OnFragmentInteractionListener, StockPromptFragment.OnFragmentInteractionListener {
+
+    StringBuilder URLBuilder = new StringBuilder("http://utdallas.edu/~John.Cole/2017Spring/");
+    final String TAG = "ASSIGNMENT2";
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager layoutManager;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_assignment2_one);
+
+        recyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
+
+        // use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        recyclerView.setHasFixedSize(true);
+
+        // use a linear layout manager
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+    }
+
+    public void onClickDisplayData(View view) {
+
+        EditText editText = findViewById(R.id.inputSymbolText);
+        String symbol = editText.getText().toString();
+        URLBuilder.append(symbol.toUpperCase());
+        URLBuilder.append(".txt");
+        String url = URLBuilder.toString();
+        new GetStockDataTask().execute(url);
+
+
+        URLBuilder = new StringBuilder("http://utdallas.edu/~John.Cole/2017Spring/");
+    }
+
+
+    class GetStockDataTask extends AsyncTask<String, Integer, ArrayList<String>>{
+
+        @Override
+        protected ArrayList<String> doInBackground(String... urls) {
+            ArrayList<String> data = new ArrayList();
+            try {
+                URL url = new URL(urls[0]);
+                Log.i(TAG, urls[0]);
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                connection.setRequestMethod("GET");
+                connection.setConnectTimeout(45000);
+                //connection.setRequestProperty("Accept", "app2");
+                connection.connect();
+                if(connection.getResponseCode() == 200){
+                    InputStream responseInputStream = connection.getInputStream();
+                    Scanner scanner = new Scanner(responseInputStream);
+                    StringBuilder builder = new StringBuilder();
+                    while(scanner.hasNext()){
+                        data.add(scanner.nextLine().replace(",", " "));
+                    }
+                    responseInputStream.close();
+                    scanner.close();
+                }else{
+                    Log.i(TAG, "error "  + connection.getResponseCode());
+                }
+
+            }catch (Exception e){
+                Log.e(TAG, e.toString());
+            }
+
+
+
+
+            return data;
+        }
+
+        protected void onPostExecute(ArrayList<String> result) {
+            String[] dataset = new String[result.size()];
+            dataset = result.toArray(dataset);
+
+            mAdapter = new MyAdapter(dataset);
+            recyclerView.setAdapter(mAdapter);
+
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+            StockDataDisplayFragment fragment = new StockDataDisplayFragment();
+            fragmentTransaction.replace(R.id.activity_assignment_2_constraint_layout, fragment);
+            fragmentTransaction.commit();
+        }
+    }
+
+
+    public void onFragmentInteraction(Uri uri){
+
+    }
+
+
+}
+
---
+++ app/src/main/java/com/example/asim/assignment2/MyAdapter.java
@@ -0,0 +1,77 @@
+package com.example.asim.assignment2;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder>{
+
+    private String[] mDataset;
+
+    // Provide a reference to the views for each data item
+    // Complex data items may need more than one view per item, and
+    // you provide access to all the views for a data item in a view holder
+    public static class MyViewHolder extends RecyclerView.ViewHolder {
+        // each data item is just a string in this case
+        public TextView dateTextView;
+        public TextView openTextView;
+        public TextView highTextView;
+        public TextView lowTextView;
+        public TextView closeTextView;
+        public TextView volumeTextView;
+        public TextView adjcloseTextView;
+        public MyViewHolder(View v) {
+
+            super(v);
+            dateTextView = v.findViewById(R.id.dateTextView);
+            openTextView = v.findViewById(R.id.openTextView);
+            highTextView = v.findViewById(R.id.highTextView);
+            lowTextView = v.findViewById(R.id.lowTextView);
+            closeTextView = v.findViewById(R.id.closeTextView);
+            volumeTextView = v.findViewById(R.id.volumeTextView);
+            adjcloseTextView = v.findViewById(R.id.adjcloseTextView);
+        }
+    }
+
+    // Provide a suitable constructor (depends on the kind of dataset)
+    public MyAdapter(String[] myDataset) {
+        mDataset = myDataset;
+    }
+
+    // Create new views (invoked by the layout manager)
+    @Override
+    public MyAdapter.MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        // create a new view
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.my_text_view, parent, false);
+
+        MyViewHolder vh = new MyViewHolder(v);
+        return vh;
+    }
+
+    // Replace the contents of a view (invoked by the layout manager)
+    @Override
+    public void onBindViewHolder(MyViewHolder holder, int position) {
+        // - get element from your dataset at this position
+        // - replace the contents of the view with that element
+        //holder.textView.setText(mDataset[position]);
+        String[] data = mDataset[position].split(" ");
+        holder.dateTextView.setText(data[0]);
+        holder.openTextView.setText(data[1]);
+        holder.highTextView.setText(data[2]);
+        holder.lowTextView.setText(data[3]);
+        holder.closeTextView.setText(data[4]);
+        holder.volumeTextView.setText(data[5]);
+        holder.adjcloseTextView.setText(data[6]);
+    }
+
+    // Return the size of your dataset (invoked by the layout manager)
+    @Override
+    public int getItemCount() {
+        return mDataset.length;
+    }
+
+
+}
